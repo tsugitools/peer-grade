886ba892b8ed4705d7b3125a85dcc56f3aff512a
Charles Severance <csev@umich.edu>
2017-08-15 15:15:42 -0400

    Switch to web components

--- analytics.php
+++ analytics.php
@@ -11,15 +11,17 @@ if ( ! $USER->instructor ) die('Must be instructor');
 
 // View
 $OUTPUT->header();
+echo('<link rel=import href="'.$CFG->staticroot.'/webcomponents/tsugi/tsugi-analytics-chart.html">'."\n");
+echo('<link rel=import href="'.$CFG->staticroot.'/webcomponents/tsugi/tsugi-analytics-script.html">'."\n");
 $OUTPUT->bodyStart();
 $OUTPUT->topNav();
 ?>
 <p>
 <input type=submit name=doCancel onclick="location='<?php echo(addSession('index.php'));?>'; return false;" value="Back"></p>
+<tsugi-analytics-chart chartid="chart_div"></tsugi-analytics-chart>
 <?php
-echo(Analytics::graphBody());
-$analytics_url = $CFG->wwwroot."/api/analytics";
+$analytics_url = addSession($CFG->wwwroot."/api/analytics");
 
 $OUTPUT->footerStart();
-echo(Analytics::graphScript(addSession($analytics_url)));
+echo('<tsugi-analytics-script chartid="chart_div" chartdata="'.$analytics_url.'"></tsugi-analytics-script>'."\n");
 $OUTPUT->footerEnd();
